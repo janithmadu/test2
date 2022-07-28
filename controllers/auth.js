600d1c5fd1e8ac8051703b28318562b3c1f09ea0
Aviblogger <avishkadevinda2015@gmail.com>
2022-07-28 12:58:22 +0530

    heroku utils spelling error

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const ErrorResponse = require('../Utils/errorResponse');
+const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
 const User = require('../models/user');
